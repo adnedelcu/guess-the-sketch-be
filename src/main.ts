0df521ecb3b8bf3d6e5e93f4cf2d8f91c599b950
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-11-28 22:41:04 +0100

    Added vercel speed insights

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
