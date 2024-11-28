c5d36aba88aa4c4af6a7274e37b8acfd9a45896a
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-11-28 22:55:01 +0100

    Hopefully fixed the deployment

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,7 @@ injectSpeedInsights();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  return app;
 }
-bootstrap();
+export default bootstrap();
