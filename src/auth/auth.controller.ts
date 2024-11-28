dc133c910364b168e2ed1b19a4d4ee0d4d28edff
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-11-28 23:23:58 +0100

    Updated auth logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,33 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from 'src/dto/login.dto';
-import { AuthGuard } from './auth.guard';
 import { RegisterDto } from 'src/dto/register.dto';
+import { Public } from 'src/decorator/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('register')
   async register(@Body() registerDto: RegisterDto) {
-    return { token: this.authService.register(registerDto) };
+    const token = await this.authService.register(registerDto);
+
+    return { token };
   }
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
-  login(@Body() loginDto: LoginDto) {
-    return { token: this.authService.login(loginDto) };
+  async login(@Body() loginDto: LoginDto) {
+    const token = await this.authService.login(loginDto);
+
+    return { token };
   }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   profile(@Request() req) {
-    return req.user;
+    return req.user._doc;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
     UsersModule,
   ],
-  controllers: [AuthController],
-  providers: [AuthService],
+  controllers: [
+    AuthController,
+  ],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    }
+  ],
 })
 export class AuthModule {}
