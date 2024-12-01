240482faba6b2998b34483e526f1dd2554a5eee5
Alexandru-Daniel Nedelcu <alexnedelcu0@gmail.com>
2024-12-01 11:59:48 +0100

    Separated socket events to rooms

--- src/rooms/rooms.gateway.ts
+++ src/rooms/rooms.gateway.ts
@@ -1,7 +1,7 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { ErrorCodes, Room, RoomType } from './schemas/room.schema';
 import { Player } from './schemas/player.schema';
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 
 @WebSocketGateway({
   cors: {
@@ -14,15 +14,35 @@ export class RoomsGateway {
 
   private readonly rooms:Map<string, RoomType> = new Map();
 
+  @SubscribeMessage('getRoom')
+  handleGetRoom(@ConnectedSocket() client: Socket, @MessageBody() payload: any): any {
+    const room = this.rooms.get(payload.code);
+    if (!room) {
+      return { error: true, errorCode: ErrorCodes.RoomNotFound, message: 'Room does not exist' };
+    }
+
+    return { error: false, room: this.rooms.get(payload.code)?.toPlain() };
+  }
+
+  @SubscribeMessage('getRooms')
+  handleGetRooms(@ConnectedSocket() client: Socket, @MessageBody() payload: any): any {
+    const rooms = ([...this.rooms.values()]).filter(room => !room.isPrivate).map(room => room.toPlain());
+
+    return { error: false, rooms };
+  }
+
   @SubscribeMessage('createRoom')
-  handleCreateRoom(client: any, payload: any): any {
+  handleCreateRoom(@ConnectedSocket() client: Socket, @MessageBody() payload: any): any {
+    console.log(payload);
     this.rooms.set(payload.code, Room.fromObject(payload.room));
+
+    client.join(payload.code);
 
     return { rooms: this.rooms };
   }
 
   @SubscribeMessage('joinRoom')
-  handleJoinRoom(client: any, payload: any): any {
+  handleJoinRoom(@ConnectedSocket() client: Socket, @MessageBody() payload: any): any {
     if (!this.rooms.has(payload.code)) {
       return { error: true, errorCode: ErrorCodes.RoomNotFound, message: 'Room does not exist' };
     }
@@ -32,15 +52,20 @@ export class RoomsGateway {
       return { error: true, errorCode: ErrorCodes.PlayerAlreadyInRoom, message: 'Player already in the room' };
     }
 
+    if (room.players.size + 1 >= room.maxPlayers) {
+      return { error: true, errorCode: ErrorCodes.MaxPlayersReached, message: 'Room is already full' };
+    }
+
     room.players.set(payload.player._id, Player.fromObject(payload.player));
 
-    this.server.emit('updateRoom', { room: room.toPlain() });
+    client.join(payload.code);
+    this.server.to(payload.code).emit('updateRoom', { room: room.toPlain() });
 
     return { error: false, message: 'Player joined the room' };
   }
 
   @SubscribeMessage('leaveRoom')
-  handleLeaveRoom(client: any, payload: any): any {
+  handleLeaveRoom(@ConnectedSocket() client: Socket, @MessageBody() payload: any): any {
     const room = this.rooms.get(payload.code);
     if (!room) {
       return { error: true, errorCode: ErrorCodes.RoomNotFound, message: 'Room does not exist' };
@@ -52,32 +77,23 @@ export class RoomsGateway {
     }
 
     room.players.delete(player._id);
-    this.rooms.set(room.code, room);
+    if (!room.players.size) {
+      this.rooms.delete(room.code);
+    } else {
+      if (player._id == room.owner._id && room.players.size) {
+        room.owner = [...room.players.values()][0];
+      }
+      this.rooms.set(room.code, room);
 
-    this.server.emit('updateRoom', { room: room.toPlain() });
+      this.server.to(payload.code).emit('updateRoom', { room: room.toPlain() });
+    }
+    client.leave(payload.code);
 
     return { error: false, message: 'Player left room' };
   }
 
-  @SubscribeMessage('getRoom')
-  handleGetRoom(client: any, payload: any): any {
-    const room = this.rooms.get(payload.code);
-    if (!room) {
-      return { error: true, errorCode: ErrorCodes.RoomNotFound, message: 'Room does not exist' };
-    }
-
-    return { error: false, room: this.rooms.get(payload.code)?.toPlain() };
-  }
-
-  @SubscribeMessage('getRooms')
-  handleGetRooms(client: any, payload: any): any {
-    const rooms = ([...this.rooms.values()]).filter(room => !room.isPrivate).map(room => room.toPlain());
-
-    return { error: false, rooms };
-  }
-
   @SubscribeMessage('toggleReady')
-  handleToggleReady(client: any, payload: any): any {
+  handleToggleReady(@ConnectedSocket() client: Socket, @MessageBody() payload: any): any {
     const room = this.rooms.get(payload.room.code);
     if (!room) {
       return { error: true, errorCode: ErrorCodes.RoomNotFound, message: 'Room does not exist' };
@@ -92,13 +108,13 @@ export class RoomsGateway {
     room.players.set(player._id, player);
     this.rooms.set(room.code, room);
 
-    this.server.emit('updateRoom', { room: room.toPlain() });
+    this.server.to(payload.code).emit('updateRoom', { room: room.toPlain() });
 
     return { error: false, room: room.toPlain(), player };
   }
 
   @SubscribeMessage('startGame')
-  handleStartGame(client: any, payload: any): any {
+  handleStartGame(@ConnectedSocket() client: Socket, @MessageBody() payload: any): any {
     const room = this.rooms.get(payload.code);
     if (!room) {
       return { error: true, errorCode: ErrorCodes.RoomNotFound, message: 'Room does not exist' };
@@ -107,13 +123,13 @@ export class RoomsGateway {
     room.hasStarted = true;
     this.rooms.set(room.code, room);
 
-    this.server.emit('updateRoom', { room: room.toPlain() });
+    this.server.to(payload.code).emit('updateRoom', { room: room.toPlain() });
 
     return { error: false, room: room.toPlain() };
   }
 
   @SubscribeMessage('updateRoomCanvas')
-  handleUpdateRoomCanvas(client: any, payload: any): any {
+  handleUpdateRoomCanvas(@ConnectedSocket() client: Socket, @MessageBody() payload: any): any {
     const room = this.rooms.get(payload.room.code);
     if (!room) {
       return { error: true, errorCode: ErrorCodes.RoomNotFound, message: 'Room does not exist' };
@@ -123,13 +139,8 @@ export class RoomsGateway {
 
     room.canvas = payload.canvas;
 
-    this.server.emit('updateRoomCanvas', { playerId: payload.playerId, room: room.toPlain() });
+    this.server.to(payload.code).emit('updateRoomCanvas', { playerId: payload.playerId, room: room.toPlain() });
 
     return { error: false, room: room.toPlain() };
-  }
-
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
   }
 }
--- src/rooms/schemas/room.schema.ts
+++ src/rooms/schemas/room.schema.ts
@@ -4,6 +4,7 @@ export enum ErrorCodes {
   RoomNotFound = 'room_not_found',
   PlayerAlreadyInRoom = 'player_already_in_room',
   PlayerNotFound = 'player_not_found',
+  MaxPlayersReached = "max_players_reached",
 }
 
 export type RoomType = {
